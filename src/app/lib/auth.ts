d60e194dc9f62303a3ade6ab1da96c4444db4ff5
Mubeen-creator <mmaj8855@gmail.com>
2025-04-04 16:39:56 +0500

    Auth option Error solved

--- src/app/lib/auth.ts
+++ src/app/lib/auth.ts
@@ -1,5 +1,5 @@
 import { getServerSession } from "next-auth/next";
-import { authOptions } from "@/app/api/auth/[...nextauth]/route";
+import { authOptions } from "./authOptions";
 import { AuthOptions } from "next-auth";
 
 export async function getSession() {
